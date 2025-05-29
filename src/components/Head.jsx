660739110a7f394b64545f0bb65451380885776a
abdujalil-nizomiddinov <n1zomiddinov571@gmail.com>
2025-05-29 11:17:45 +0500

    sl1

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,9 +1,43 @@
 import Navbar from "./Navbar.jsx";
+import style from "./css/Head.module.css";
+import HeadImages from "../assets/head.png";
 
 const Head = () => {
   return (
     <div>
       <Navbar />
+      <header className={style.header}>
+        <div className={style.hero}>
+          <img className={style.img} src={HeadImages} alt="foto" />
+        </div>
+        <div className={style.title}>
+          <h1>
+            Kembang <br /> Flower Mantap
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. In sed nam
+            rem quos pariatur architecto corrupti enim, earum cum temporibus
+            excepturi quia perspiciatis est similique alias consequatur a quas
+            impedit!
+          </p>
+          <div className={style.next}>
+            <div className={style.left}>
+              <p>
+                <i className="fa-solid fa-arrow-left"></i>
+                <span>Previous</span>
+              </p>
+              <h4>Rahasia Plant</h4>
+            </div>
+            <div className={style.right}>
+              <p>
+                <span>Next</span>
+                <i className="fa-solid fa-arrow-right"></i>
+              </p>
+              <h4>Kaktus Plant</h4>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,22 @@
 import style from "./css/Navbar.module.css";
 
-const Navbar = () => {
+export const Navbar = () => {
   return (
     <nav className={style.nav}>
-      <img src="/Logo.svg" alt="logo" className={style.logo} />
-
-      <ul className={style.menu}>
-        <li>
+      <a className={style.logo} href="#">
+        <img src="../../public/Logo.svg" alt="logo" />
+      </a>
+      <div className={style.nav_item}>
+        <div className={style.icon}>
           <i className="fa-solid fa-magnifying-glass"></i>
-        </li>
-        <li>
           <i className="fa-solid fa-cart-shopping"></i>
-        </li>
-        <li>
-          <span className={style.link}>Sign Up</span>
-        </li>
-        <li>
-          <button className={style.btn}>Sign In</button>
-        </li>
-      </ul>
+        </div>
+        <button className={style.btn1}>Sign Up</button>
+        <button className={style.btn2}>Sign In</button>
+      </div>
+      <div className={style.nav_bars}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
     </nav>
   );
 };
